649e376a6abc11cfbaeb332648879006bd4604d3
arifislam121416 <ardairyhelthcare@gmail.com>
2026-03-31 00:48:00 +0600

    Google font Manrope add

--- src/component/DigitalTools/DigitalTools.jsx
+++ src/component/DigitalTools/DigitalTools.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 const DigitalTools = () => {
     return (
         <div>
-            
+            <div className='py-4 text-center'>
+                <h1 className='text-4xl font-bold '>Premium Digital Tools</h1>
+                <p>Choose from our curated collection of premium digital products designed to boost your productivity and creativity.</p>
+            </div>
+            <div>
+                
+            </div>
         </div>
     );
 };
